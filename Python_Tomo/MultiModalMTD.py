db7354d24e03e8ebbb9f374bd79cbcfd0200626a
Nicolas Verrier <nicolas.verrier@uha.fr>
2020-07-28 14:23:01 +0200

    Ajout fonctions de filtrage pour MTD multimodale

---
+++ Python_Tomo/MultiModalMTD.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 28 13:25:31 2020
+
+@author: Nicolas Verrier
+"""
+import numpy as np
+
+def darkfield(Spectrum, FiltRadius, SpecCoord):
+    """
+    Emulation of darkfield measurement from holographic acquisitions
+
+    Parameters
+    ----------
+    Spectrum : complex128
+        Spectrum of the field to be processed.
+    FiltRadius : int
+        Radius of the filter.
+    SpecCoord : int32
+        Coordinates of the specular spot.
+
+    Returns
+    -------
+    Spectrum : complex128
+        Processed filter.
+
+    """
+    kx, ky = np.meshgrid(np.arange(-int(Spectrum.shape[1]/2),int(Spectrum.shape[1]/2)), np.arange(-int(Spectrum.shape[0]/2),int(Spectrum.shape[0]/2)))
+    Spectrum[(kx-SpecCoord[1])**2+(ky-SpecCoord[0])**2<FiltRadius**2] = 0
+    return Spectrum
