5644606df725e3b54782910578ab398256113af3
Nicolas Verrier <nicolas.verrier@uha.fr>
2020-11-23 16:55:44 +0100

    Ajout simulation hologrammes (à débugguer)

--- Python_Tomo/SimuTomo.py
+++ Python_Tomo/SimuTomo.py
@@ -6,6 +6,55 @@ Created on Thu Jun 11 16:31:30 2020
 """
 import numpy as np
 
+def OTF_Cap(dimHolo, NA_ill, nimm, SpecCoord):
+    """
+    OTF simulation for a single cap of sphere
+
+    Parameters
+    ----------
+    dimHolo : int
+        Lateral dimension of.
+    NA_ill : float
+        Numerical aperture of the collection objective.
+    nimm : float
+        Immersion medium.
+    SpecCoord : (int,int)
+        (y,x) Coordinates of the specular spot.
+
+    Returns
+    -------
+    OTF_Simu : int32
+        Optical Transfert Function.    
+
+    """
+    REwald = dimHolo/2
+    OTF_Simu = np.zeros((2*dimHolo, 2*dimHolo, 2*dimHolo))
+    
+    # Maximal frequency   
+    Fmax = np.round(REwald*NA_ill/nimm)
+    
+    # Definition of diffracted wavevectors
+    kdx = np.arange(-Fmax,Fmax)
+    kdy = np.arange(-Fmax,Fmax)
+    KDX, KDY = np.meshgrid(kdx, kdy)
+    
+    # Illumination wavevectors
+    KIX = SpecCoord[1]
+    KIY = SpecCoord[0]
+    KIZ = np.round(np.sqrt(REwald**2-KIX**2-KIY**2))
+    
+    index = np.where(REwald**2-KDX**2-KDY**2 >= 0)
+    KDZ = np.zeros(KDX.shape)
+    KDZ[index] = np.round(np.sqrt(REwald**2-KDX[index]**2-KDY[index]**2))
+        
+    KOX = KDX - KIX
+    KOY = KDY - KIY
+    KOZ = KDZ - KIZ
+
+    OTF_Simu[np.int32(KOY[index]+dimHolo),np.int32(KOX[index]+dimHolo),np.int32(KOZ[index]+dimHolo)]=1
+    return OTF_Simu
+    
+
 def OTF_Flower(dimHolo, NA_ill, nimm, nbangle):
     """
     OTF simulation with a flower illumination swweping
@@ -94,3 +143,52 @@ def BeadSimu(Radius,dimHolo,nimm,nbead,kappa):
     Refraction = Refraction * (nbead-nimm) + nimm
     Bead = Refraction + 1j* Absorption
     return Bead
+
+def Calc_TF_Holo(TF_vol3D, TF_Holo, fd_m, sdz_m, dx_m, dy_m, Nmax, k_inc, R_Ewald, lambda_v, n0):
+    """
+    Projection of the cap of sphere on a 2D plane
+
+    Parameters
+    ----------
+    TF_vol3D : complex128
+        3D spectrum of the object.
+    TF_Holo : complex128
+        2D spectrum of the object.
+    fd_m : int
+        Masked coordinates of the diffracted vectors.
+    sdz_m : float
+        Normalization of the diffracted vectors.
+    dx_m : int
+        Abcissa of the filtered coordinate.
+    dy_m : int
+        Ordonnae of the filtered coordinate.
+    Nmax : int
+        Maximal spatial frequency.
+    k_inc : int32
+        Array with the y, x coordinates of the specular spot.
+    REwald : float64
+        Radius of the Ewald sphere.
+    lambda_v : float64
+        Wavelength of light in vacuum.
+    n0 : float64
+        Refractive index of the immersion medium.
+
+    Returns
+    -------
+    TF_Holo : complex128
+        2D spectrum of the object.
+
+    """
+    fi = np.array([k_inc[0], k_inc[1], np.round(np.sqrt(R_Ewald**2 - k_inc[0]**2 - k_inc[1]**2))])
+    
+    kv = 2*np.pi/lambda_v
+    k0 = kv * n0
+
+    cteInd2Pot = -2*kv**2*n0
+    ctePot2UBorn = -1j*np.pi/k0
+    cteNormalisation = -1/(2*np.pi)
+
+    fobj_m = (fd_m-np.round(fi[:, np.newaxis])/2).astype(int)
+    
+    TF_Holo[dx_m+Nmax,dy_m+Nmax] = (cteInd2Pot * ctePot2UBorn * cteNormalisation)/sdz_m * TF_vol3D[fobj_m[0,:], fobj_m[1,:], fobj_m[2,:]]
+    return TF_Holo
---
+++ Python_Tomo/SimulationHologrammes.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: Nicolas Verrier
+"""
+import SimuTomo as st
+# import matplotlib.pyplot as plt
+import time
+import numpy as np
+import FileTools as ft
+from scipy.fftpack import fftn,ifftn,fftshift
+import Retropropagation as rp
+
+NA_ill = 1.4
+nimm = 1.518
+nbead = 1.55
+nbangle = 50
+kappa = 0.01
+Radius = 20
+DOSSIERDATA = "C:/Users/p1600109/Documents/Recherche/MatlabTomo/SimuHolo/"
+CHEMINPARAM = f"{DOSSIERDATA}Param.txt"
+
+# Path to the parameter file, and parameters reading
+DARKFIELD = False
+PHASECONTRAST = False
+REWALD = float(ft.readvalue(CHEMINPARAM, 'REwald'))
+NB_ANGLE = int(ft.readvalue(CHEMINPARAM, 'nb_angle'))
+FMAXHOLO = int(ft.readvalue(CHEMINPARAM, 'fmaxHolo'))
+DIMHOLO = int(ft.readvalue(CHEMINPARAM, 'dimHolo'))
+PIXTHEO = float(ft.readvalue(CHEMINPARAM, 'pixTheo'))
+UBornPitch = 1/(2*FMAXHOLO*PIXTHEO)
+NB_HOLO = NB_ANGLE
+
+# Paths to the real, and imaginary parts of the field
+CHEMIN_RE_UBORN = f"{DOSSIERDATA}ReBorn_{DIMHOLO}.tiff"
+CHEMIN_IM_UBORN = f"{DOSSIERDATA}ImBorn_{DIMHOLO}.tiff"
+
+# Path to the specular coordinates
+SpecCoordPath = f"{DOSSIERDATA}/Centres_{DIMHOLO}.txt"
+fi = rp.Calc_fi(SpecCoordPath, NB_ANGLE, DIMHOLO)
+
+total_time = time.time()
+start_time = time.time()
+if NA_ill/nimm>=1:
+    print(f"Simulation impossible: nimm >= {NA_ill}")
+else:
+    start_time = time.time()
+    Bille = st.BeadSimu(Radius, DIMHOLO, nimm, nbead, kappa)
+    print(f"Bead simulation time : {np.round(time.time() - start_time,decimals=2)} seconds")    
+    
+start_time = time.time()    
+TomoSpectrum = fftshift(fftn(Bille))
+print(f"3D-FFT computation time : {np.round(time.time() - start_time,decimals=2)} seconds")
+fdm, sdzm, dxm, dym = rp.Calc_fd(FMAXHOLO, REWALD)
+
+HOLO = np.zeros((DIMHOLO, DIMHOLO, NB_HOLO), dtype = np.complex128)
+start_time = time.time()
+for cpt in range(NB_HOLO):
+    if cpt % 100 == 0:
+        print(f"Hologram = {cpt} out of {NB_HOLO}")
+    TF_Holo = np.zeros((DIMHOLO, DIMHOLO), dtype = np.complex128)    
+    k_inc = np.array([fi[0, cpt], fi[1, cpt]])
+    TF_Holo = st.Calc_TF_Holo(TomoSpectrum, TF_Holo, fdm, sdzm, dxm, dym, FMAXHOLO, k_inc, REWALD, 632.8e-9, nimm)
+    decal = np.array([-k_inc[0], -k_inc[1]]).astype(int)
+    TF_Holo_r = rp.decal_TF_holo(TF_Holo, decal, TF_Holo.shape[0])
+    HOLO[:, :, cpt] = ifftn(fftshift(TF_Holo_r))*(UBornPitch**2*FMAXHOLO**2)
+    # plt.imshow(np.abs(HOLO), cmap="gray")
+    # plt.show()
+print(f"Simulation time for {NB_HOLO} holograms: {np.round(time.time() - start_time,decimals=2)} seconds")
+print("")
+ft.SAVtiffCube(CHEMIN_RE_UBORN, HOLO.real, PIXTHEO*1e6)
+ft.SAVtiffCube(CHEMIN_IM_UBORN, HOLO.imag, PIXTHEO*1e6)
+print(f"Execution time: {np.round(time.time() - total_time,decimals=2)} seconds")
